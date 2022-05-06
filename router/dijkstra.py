61e3874ec740afd3a3e3e82d2aa7d6e56c9aa954
Khyati <92055071+khyati-dp@users.noreply.github.com>
2022-05-06 00:44:33 -0400

    Create dijkstra.py

---
+++ router/dijkstra.py
@@ -0,0 +1,45 @@
+from priodict import priorityDictionary
+
+def Dijkstra(G,start,end=None):
+    D = {}  # dictionary of final distances
+    P = {}  # dictionary of predecessors
+    Q = priorityDictionary()   # est.dist. of non-final vert.
+    Q[start] = 0
+    
+    for v in Q:
+        D[v] = Q[v]
+        if v == end: break
+        
+        for w in G[v]:
+            vwLength = D[v] + G[v][w]
+            if w in D:
+                if vwLength < D[w]:
+                    raise ValueError("Dijkstra: found better path to already-final vertex")
+            elif w not in Q or vwLength < Q[w]:
+                Q[w] = vwLength
+                P[w] = v
+    
+    return (D,P)
+            
+def shortestPath(G,start,end):
+    """
+    Find a single shortest path from the given start vertex
+    to the given end vertex.
+    The input has the same conventions as Dijkstra().
+    The output is a list of the vertices in order along
+    the shortest path.
+    """
+
+    D,P = Dijkstra(G,start,end)
+    Path = []
+    while 1:
+        Path.append(end)
+        if end == start: break
+        end = P[end]
+    Path.reverse()
+    return Path
+
+
+#print shortestPath(graph, 'N1','N13')
+#print shortestPath(graph, 'N1','N12')
+#print shortestPath(graphnew, '101','102')
