200ebbe8317cd55f532b85ccba2d1ea4c2a7a46a
Khyati <92055071+khyati-dp@users.noreply.github.com>
2022-05-07 12:36:51 -0400

    Update host.py

--- host/host.py
+++ host/host.py
@@ -1,76 +1,134 @@
-import sys
-sys.path.append('../')
-import commonFunctions
-import hostFunctions
-import time
-import struct
-import os, subprocess
-from socket import *
+#!/usr/bin/python
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import lg, setLogLevel, info
+from mininet.cli import CLI
+from mininet.node import Node
+from mininet.link import Link, TCLink 
+from mininet.util import dumpNodeConnections
+import threading
+import sys, time
 
-if __name__ == "__main__":
+import cleanup
+from printer import *
+from packet import *
 
-    #read the hosts's ID from the command line input -id
-    myID = int(commonFunctions.getID())
-    #host maintains and broadcasts it's single link state
-    #does not maintain data about the rest of the network
-    myLink = {str(myID): []}
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
+
+class topoFinal( Topo ):
+    #Custom topology Example 1
+    #Create a Mininet Environment
+    def build( self, **_opts ):
+
+        info( "*** Creating Routers\n" )
+
+        routers = [ self.addNode( 'r%d' % n, cls=LinuxRouter,
+                    ip='192.168.1.%d/24' % n ) 
+                    for n in range(201,206) ]
+
+        info( "*** Creating Router Links\n" )
+        self.addLink( routers[routers.index('r201')], routers[routers.index('r202')],
+            intfName1='r201-eth0' )
+        self.addLink( routers[routers.index('r201')], routers[routers.index('r203')],
+            intfName1='r201-eth1' )
+        self.addLink( routers[routers.index('r203')], routers[routers.index('r204')],
+            intfName1='r203-eth1' )
+        self.addLink( routers[routers.index('r203')], routers[routers.index('r205')],
+            intfName1='r203-eth2' )
+        self.addLink( routers[routers.index('r204')], routers[routers.index('r205')],
+            intfName1='r204-eth1' )
+        #self.addLink( routers[routers.index('r205')], routers[routers.index('r206')],
+            ##intfName1='r205-eth2' )
+
+        info( "*** Creating Hosts\n" )
+        hosts = [ self.addHost( 'h%d' % n, 
+                    ip='192.168.1.%d/24' % n )
+                    for n in range(101,105) ]
+
+        info( "*** Creating Host Links\n" )
+        self.addLink( hosts[hosts.index('h101')], routers[routers.index('r201')],
+            intfName1='h101-eth0' )    
+        self.addLink( hosts[hosts.index('h102')], routers[routers.index('r202')],
+            intfName1='h102-eth0' )
+        self.addLink( hosts[hosts.index('h103')], routers[routers.index('r204')],
+            intfName1='h103-eth0' )
+        self.addLink( hosts[hosts.index('h104')], routers[routers.index('r205')],
+            intfName1='h104-eth0' )
     
-    #Maybe, need function here to setup particular host as the
-    #broadcast node or not
-
-    #create hello packet
-    pkttype = 0x01
-    src = myID
-    #print("src is : ".format(src))
-    seq = 0x01
-    hello = struct.pack('BBB', pkttype, seq, src)
-    #hello = hostFunctions.createHelloPacket()
-    #Sends hello on its broadcast IP address
-
-    data, addr, myLink = hostFunctions.sendHelloPacket(commonFunctions.convertID(myID), hello, '192.168.1.255', myLink, myID)
+def run():
     
-    hostFunctions.broadcastLinkState(myID, '192.168.1.255', myLink)
+    #Setup topo and list information
+    topo = topoFinal()
+    net = Mininet( topo=topo, controller=None, link=TCLink )
+    net.start()
 
-    while True:
-        """
-        #**** for testing, remove before flights ******
-        #create hello packet
-        pkttype = 0x08
-        src = myID
-        #print("src is : ".format(src))
-        seq = 0x01
-        hello = struct.pack('BBB', pkttype, seq, src)
-        
-        my_socket = socket(AF_INET, SOCK_DGRAM)
-        my_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) 
-        my_socket.sendto(hello, ('192.168.1.255', 8888))
-        my_socket.close()
-        print("Sent ACK Packet")
-        time.sleep(1)
-        """
+    info( "*** Setup routes on all devices" )
+    #Host routes
+    net['h101'].cmd( 'route add -net 192.168.1.0/24 gw {}'.format(net['r201'].IP()) ) 
+    net['h102'].cmd( 'route add -net 192.168.1.0/24 gw {}'.format(net['r202'].IP()) ) 
+    net['h103'].cmd( 'route add -net 192.168.1.0/24 gw {}'.format(net['r204'].IP()) ) 
+    net['h104'].cmd( 'route add -net 192.168.1.0/24 gw {}'.format(net['r205'].IP()) ) 
+   
+    #Router r201 routes
+    net['r201'].cmd( 'ip route add {}/32 dev r201-eth0'.format(net['r202'].IP()) )
+    net['r201'].cmd( 'ip route add {}/32 dev r201-eth1'.format(net['r203'].IP()) )
+    net['r201'].cmd( 'ip route add {}/32 dev r201-eth2'.format(net['h101'].IP()) )
 
-        data = hostFunctions.receive_packet('0.0.0.0', 8888)
-        #NEED to update function to move on after receiving ACK
+    #Router r202 routes
+    net['r202'].cmd( 'ip route add {}/32 dev r202-eth0'.format(net['r201'].IP()) )
+    net['r202'].cmd( 'ip route add {}/32 dev r202-eth1'.format(net['h102'].IP()) )
 
-        packetType = hostFunctions.decodePktType(data)
+    #Router r203 routes
+    net['r203'].cmd( 'ip route add {}/32 dev r203-eth0'.format(net['r201'].IP()) )
+    net['r203'].cmd( 'ip route add {}/32 dev r203-eth1'.format(net['r204'].IP()) )
+    net['r203'].cmd( 'ip route add {}/32 dev r203-eth2'.format(net['r205'].IP()) )
 
-        if(packetType[0] == 8):
-            print("I'm a host and got some data")
+    #Router r204 routes
+    net['r204'].cmd( 'ip route add {}/32 dev r204-eth0'.format(net['r203'].IP()) )
+    net['r204'].cmd( 'ip route add {}/32 dev r204-eth1'.format(net['r205'].IP()) )
+    net['r204'].cmd( 'ip route add {}/32 dev r204-eth2'.format(net['h103'].IP()) )
+    
+    #Router r205 routes
+    net['r205'].cmd( 'ip route add {}/32 dev r205-eth0'.format(net['r203'].IP()) )
+    net['r205'].cmd( 'ip route add {}/32 dev r205-eth1'.format(net['r204'].IP()) )
+    net['r205'].cmd( 'ip route add {}/32 dev r205-eth2'.format(net['h104'].IP()) ) 
+    
+    #Router r206 routes
+    #net['r206'].cmd( 'ip route add {}/32 dev r206-eth0'.format(net['r205'].IP()) )
+    #net['r206'].cmd( 'ip route add {}/32 dev r206-eth1'.format(net['h104'].IP()) )
+    
+    
+    
+    #DEBUGGING INFO    
+    info( "\n\n" )
+    info( "*** List host IPs\n" )
+    get_host_ips(net)
+    
+    info( "*** List all routes\n" )
+    for node in net.hosts:
+      print(node.cmd('route -n'))
+      
+    info( "*** Dumping host connections\n" )
+    dumpNodeConnections(net.hosts)
+    
+    info( "*** Pinging all devices\n" )
+    net.pingAll()
+    
+    CLI( net )
+    net.stop()
 
-    #create data packet
-    pktType = 0x07
-    src = myID
-    seq = 0x01
-    #k out of n destinations 1-3
-    ndest = 3
-    data = "This is a {} out of 3 multicast message".format(ndest)
-    #rdest, dest1-3, will not be set when host sends pkt
-    #Core router will determine those values
-    #At this point the host should know the attached router
-    #Does it make sense to send to that router or still broadcast like hellopkt?
-    rdest = 0
-    dest1 = 0
-    dest2 = 0
-    dest3 = 0
-    datapkt = commonFunctions.createDataPacket(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data)
-    #code to send datapkt
+if __name__ == '__main__':
+    cleanup.cleanup()
+    setLogLevel( 'info' )  # for CLI output
+    run()
