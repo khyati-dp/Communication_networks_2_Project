b31865bec3d9c0f3a927541a9ea4c96ae24ec8ef
Khyati <92055071+khyati-dp@users.noreply.github.com>
2022-05-07 13:11:21 -0400

    Update commonFunctions.py

--- host/commonFunctions.py
+++ host/commonFunctions.py
@@ -1,69 +1,47 @@
-import sys
-sys.path.append('../')
-import commonFunctions
-import hostFunctions
-import time
+import subprocess
 import struct
-import os, subprocess
-from socket import *
+import json
 
-if __name__ == "__main__":
+def getID():
+    p = subprocess.Popen("ifconfig | grep 192 | awk '{print $2}' | awk -F. '{print $4}'", stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    return output
 
-    #read the hosts's ID from the command line input -id
-    myID = int(commonFunctions.getID())
-    #host maintains and broadcasts it's single link state
-    #does not maintain data about the rest of the network
-    myLink = {str(myID): []}
-    
-    #Maybe, need function here to setup particular host as the
-    #broadcast node or not
+def convertID(id):
+    return "192.168.1." + str(id)
 
-    #create hello packet
-    pkttype = 0x01
-    src = myID
-    #print("src is : ".format(src))
-    seq = 0x01
-    hello = struct.pack('BBB', pkttype, seq, src)
-    #hello = hostFunctions.createHelloPacket()
-    #Sends hello on its broadcast IP address
+def createDataPacket(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data):
+    """
+    Create data packet
+    """
+    pktFormat = "BBBBBBBBB"
+    dataPkt = struct.pack(pktFormat, pktType, seq, src, len(data), int(ndest), int(rdest), int(dest1), int(dest2), int(dest3))+data
+    return dataPkt
 
-    data, addr, myLink = hostFunctions.sendHelloPacket(commonFunctions.convertID(myID), hello, '192.168.1.255', myLink, myID)
-    
-    hostFunctions.broadcastLinkState(myID, '192.168.1.255', myLink)
+def decodeDataPkt(pkt):
+    """
+    Decodes Data Packet as byte, int, int, byte and 
+    assumes that data has been concatenaged to the end of the 
+    incoming packet
 
-    #print("DATA: {} ADDR: {} MYLINK: {}".format(data, addr, myLink))
+    Arguments:
+        pkt {packed struct} -- Data that needs to be unpacked
 
-    while True:
-        """
-        try:
-            while True:
-                receivedPkt, addr = hostFunctions.receive_packet('0.0.0.0', 8888)
+    Returns:
+        seq, src, length, ndest, rdest, dest1, dest2, dest3 -- Returns all unpacked data except pktType
+    """
+    pktFormat = "BBBBBBBBB"
+    pktSize = struct.calcsize(pktFormat)
+    pktHeader = pkt[:pktSize]
 
-                packetType = hostFunctions.decodePktType(receivedPkt)
+    pktType, seq, src, length, ndest, rdest, dest1, dest2, dest3 = struct.unpack(pktFormat, pktHeader)
+    data = pkt[pktSize:]
 
-                if(packetType == 8):
-                    print("Received dataACK")
+    return seq, src, ndest, rdest, dest1, dest2, dest3, data
 
-        #Interupting the code by hitting CTRL+C lets you send a data packet on demand
-        except KeyboardInterrupt:
-        """
-        #k out of n destinations 1-3
-        ndest = input("Enter K out of N (1-3):")
-        print("Multicasting to {} out of 3".format(ndest))
-        #create data packet
-        pktType = 0x07
-        src = myID
-        seq = 0x01
-        data = "This is a {} out of 3 multicast message".format(ndest)
-        #rdest, dest1-3, will not be set when host sends pkt
-        #Core router will determine those values
-        #At this point the host should know the attached router
-        #Does it make sense to send to that router or still broadcast like hellopkt?
-        rdest = 0
-        dest1 = 0
-        dest2 = 0
-        dest3 = 0
-        datapkt = commonFunctions.createDataPacket(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data)
-        #hostFunctions.sendData(datapkt, 201, myID)
-        hostFunctions.sendData(datapkt, myLink[str(myID)][0], myID)
-        print("Sent Data Packet with information {} {} {} {} {} {} {} {} {}".format(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data))
+def getNextHop(myID, destID):
+    #print("myID: {} destID: {}".format(myID,destID))
+    with open(str(myID) + '.json', 'r') as f:
+        routingTable = json.load(f)
+    #print("Next hop is {}".format(routingTable['destination'][str(destID)]['path'][0]))
+    return routingTable['destination'][str(destID)]['path'][0]
