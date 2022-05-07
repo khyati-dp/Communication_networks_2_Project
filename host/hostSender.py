94ae3ccf1adcc93a2fda8fcdcba018b66431cb4e
Khyati <92055071+khyati-dp@users.noreply.github.com>
2022-05-07 12:55:28 -0400

    Update hostSender.py

--- host/hostSender.py
+++ host/hostSender.py
@@ -31,50 +31,39 @@ if __name__ == "__main__":
     
     hostFunctions.broadcastLinkState(myID, '192.168.1.255', myLink)
 
+    #print("DATA: {} ADDR: {} MYLINK: {}".format(data, addr, myLink))
+
     while True:
-        """
-        #**** for testing, remove before flights ******
-        #create hello packet
-        pkttype = 0x08
+        '''
+        try:
+            while True:
+                receivedPkt, addr = hostFunctions.receive_packet('0.0.0.0', 8888)
+
+                packetType = hostFunctions.decodePktType(receivedPkt)
+
+                if(packetType == 8):
+                    print("Received dataACK")
+
+        #Interupting the code by hitting CTRL+C lets you send a data packet on demand
+        except KeyboardInterrupt:
+        '''
+        #k out of n destinations 1-3
+        ndest = input("Enter K out of N (1-3):")
+        print("Multicasting to {} out of 3".format(ndest))
+        #create data packet
+        pktType = 0x07
         src = myID
-        #print("src is : ".format(src))
         seq = 0x01
-        hello = struct.pack('BBB', pkttype, seq, src)
-        
-        my_socket = socket(AF_INET, SOCK_DGRAM)
-        my_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) 
-        my_socket.sendto(hello, ('192.168.1.255', 8888))
-        my_socket.close()
-        print("Sent ACK Packet")
-        time.sleep(1)
-        """
-
-        receivedPkt, addr = hostFunctions.receive_packet('0.0.0.0', 8888)
-        #NEED to update function to move on after receiving ACK
-
-        packetType = hostFunctions.decodePktType(receivedPkt)
-
-        if(packetType == 7):
-            seq, src, ndest, rdest, dest1, dest2, dest3, data = commonFunctions.decodeDataPkt(receivedPkt)
-            print(data)
-            time.sleep(1)
-            hostFunctions.sendDataACK(addr[0])
-
-
-    #create data packet
-    pktType = 0x07
-    src = myID
-    seq = 0x01
-    #k out of n destinations 1-3
-    ndest = 3
-    data = "This is a {} out of 3 multicast message".format(ndest)
-    #rdest, dest1-3, will not be set when host sends pkt
-    #Core router will determine those values
-    #At this point the host should know the attached router
-    #Does it make sense to send to that router or still broadcast like hellopkt?
-    rdest = 0
-    dest1 = 0
-    dest2 = 0
-    dest3 = 0
-    datapkt = commonFunctions.createDataPacket(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data)
-    #code to send datapkt
+        data = "This is a {} out of 3 multicast message".format(ndest)
+        #rdest, dest1-3, will not be set when host sends pkt
+        #Core router will determine those values
+        #At this point the host should know the attached router
+        #Does it make sense to send to that router or still broadcast like hellopkt?
+        rdest = 0
+        dest1 = 0
+        dest2 = 0
+        dest3 = 0
+        datapkt = commonFunctions.createDataPacket(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data)
+        #hostFunctions.sendData(datapkt, 201, myID)
+        hostFunctions.sendData(datapkt, myLink[str(myID)][0], myID)
+        print("Sent Data Packet with information {} {} {} {} {} {} {} {} {}".format(pktType, seq, src, ndest, rdest, dest1, dest2, dest3, data))
